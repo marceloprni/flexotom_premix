e5f2574a0c80739426fc4d20a3577fec53385bee
Marcelo Perini <marcelo.perini@wcvengenharia.com.br>
2025-11-28 16:48:18 -0300

    vr28112025:1628

--- public/js/cadastroLote/cadastroLote.js
+++ public/js/cadastroLote/cadastroLote.js
@@ -28,24 +28,13 @@ var tableCadastroLote;
 
 
 /* FUNÇÕES DE INICIOS PARA CARREGAR DADOS */
-function createTable() {
+function createComponent() {
     
     axios.get("/cadastroLote/dadosLote").then(response => {
         //APAGA DADOS DO ARRAY
         Lote = []
         LoteTotal = []
         console.log(response)
-
-        // Configura o evento de focus uma única vez
-
-        /*
-        $('#materiaPrima').off('focus').on('focus', function () {
-            if (!optionsCarregadas) {
-                preencherSelect("materiaPrima", response.data.insumoAtivo);
-                optionsCarregadas = true; // garante que carregue só uma vez
-            }
-        });
-        */
        
         for(let a of response.data.insumoAtivo) {
             InsumoTotal.push({id: a.id, text: a.Descricao})
@@ -59,43 +48,7 @@ function createTable() {
         });
         // APAGADO OS DADOS SELECIONADO
         $(selectMateria).val(null).trigger('change');
-        
-        for(let b of response.data.loteAtivo){
-            Lote.push([b.Lote, b.MateriaPrimaInsumo]);
-            LoteTotal.push( [b.Lote, b.MateriaPrimaIdInsumo, b.MateriaPrimaInsumo, b.Barcode]);
-        }
-       
-        tableCadastroLote = jQuery('#tabela-CadastroLote').DataTable(
-            {
-                pagingType: 'numbers',
-                pageLength : 4,
-                bLengthChange: false,
-                lengthChange: false, 
-                bFilter: true,
-                data: Lote,
-                columns: [{title: "LOTE"}, {title: "DESCRIÇÃO"}],
-                ordering: false,
-                 language: {
-                    info: " " 
-                }
-            });
-        
-        
-        jQuery('#tabela-CadastroLote tbody').on('click', 'tr', function () 
-        {
-            var tr = jQuery(this).closest('tr');
-            var row = tableCadastroLote.row(tr);
-            /*Check if row is empty*/
-            if (typeof row.data() === 'undefined'){return null;}
-            /*Selected row*/
-            if (jQuery(this).hasClass('tableSelected')){
-                jQuery(this).removeClass('tableSelected');
-            }
-            else {
-                tableCadastroLote.$('tr.tableSelected').removeClass('tableSelected');
-                jQuery(this).addClass('tableSelected');
-            }   
-        });
+    
 
     }).catch(error => {
         console.log(error);
@@ -163,53 +116,7 @@ btnModalImprimir.onclick = () => {
     imprimirModal('barCodeModal')
 };
 
-btnDeleta.onclick = (event) => {
- 
-    event.preventDefault();
-
-    let selectedRow = tableCadastroLote.row('.tableSelected');
-
-    if (selectedRow.length === 0) {
-        alert("Selecione uma materia prima para deletar.");
-        return;
-    }
-
-    let deleteLote = selectedRow.data()[0];
-    let deleteMateriaPrima = selectedRow.data()[1];
-
-    jQuery('#delete_lote').val(deleteLote);
-    jQuery('#delete_materia').val(deleteMateriaPrima);
-    jQuery('#deleteModal').modal('show');
-
-}
-
-
-btnModalDeletar.onclick = (event) => {
-    event.preventDefault();
-    let selectedRow = tableCadastroLote.row('.tableSelected');
-    let deleteLote = selectedRow.data()[0];
-
-    axios.delete(`/cadastroLote/${deleteLote}`).then(response => {
-            console.log(response.data)
-            if(response.status == 201){
-                jQuery('#deleteModal').modal('hide');
-                jQuery('#messageDivChildren').css({"background":"#E5192E", "border": "2px solid #fff", "color": "#fff"});
-                jQuery('#message').modal('show');
-                jQuery('#messageText').text(response.data.message);
-                setTimeout(() => {
-                    window.location.reload(); 
-                },800);     
-            }
-        }).catch(error => {
-                const messagem = JSON.parse(error.request.responseText);
-                jQuery('#adicionar-modal').modal('hide');
-                jQuery('#messageDivChildren').css({"background":"#ffc107", "border": "2px solid #fff"});
-                jQuery('#message').modal('show');
-                jQuery('#messageText').text(messagem.erro);
-    }) 
-
-}
 
 
 /* INICIA A PAGINA */
-createTable();
+createComponent();
